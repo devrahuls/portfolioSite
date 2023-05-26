19cf73a80e978aecb53e8e5ed8f0a262f97830cd
rahull56 <printfrahulsaw@gmail.com>
2023-05-26 20:03:23 +0530

    some changes

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,8 +1,7 @@
-import Gymate from "../images/gymate-home.webp";
-import Raouf from "../images/ecom.webp";
 import ProBox from "./ProBox";
 import CarRental from "../images/car-rental-full.webp";
-import CoinDom from "../images/landing_page.png";
+import memeGenerator from "../images/meme-generator.webp";
+import LandingPage from "../images/landing_page.png";
 
 function Projects() {
   return (
@@ -13,9 +12,22 @@ function Projects() {
             <p>portfolio</p>
             <h3>Each project is a unique piece of development 🧩</h3>
             <div className="projects-grid">
+
+            <ProBox
+                title="Car Rental"
+                img={CarRental}
+                description="A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars."
+                techno1="React"
+                techno2="Tailwind CSS"
+                code="https://github.com/devrahuls/carRental"
+                demo="https://car-rental.devrahuls.vercel.app/"
+                scrollY="-89%"
+                icon="🚗"
+              />
+
               <ProBox
                 title="Meme Generator"
-                img={CarRental}
+                img={memeGenerator}
                 description="It's a Meme Generator app where users can create their own meme with pre-defined meme templates which is passing by Imgflip API."
                 techno1="React"
                 techno2="Vanilla CSS"
@@ -27,7 +39,7 @@ function Projects() {
 
               <ProBox
                 title="Landing Page"
-                img={CoinDom}
+                img={LandingPage}
                 description="Check out my personal project's landing page built with Tailwind CSS - a modern, minimalist design with seamless functionality."
                 techno1="React"
                 techno2="Tailwind"
@@ -38,30 +50,6 @@ function Projects() {
                 cName="reversed-proj"
               />
 
-              <ProBox
-                title="gymate"
-                img={Gymate}
-                description="A gym website is a comprehensive resource for fitness information, class schedules, membership options, and tools to help users achieve their fitness goals."
-                techno1="React"
-                techno2="Tailwind CSS"
-                code="https://github.com/stefvndev/Gymate"
-                demo="https://gymate-iota.vercel.app/"
-                scrollY="-89%"
-                icon="🏋️"
-              />
-
-              {/* <ProBox
-                title="Raouf Ecommerce"
-                img={Raouf}
-                description="With a focus on simplicity and clean design, this store prioritize user experience, making it easy for customers to find and purchase the products they need."
-                techno1="React"
-                techno2="Vanila CSS"
-                code="https://github.com/stefvndev/Minimalist-E-commerce"
-                demo="https://minimalist-e-commerce.vercel.app/"
-                scrollY="-71%"
-                icon="🛒"
-                cName="reversed-proj"
-              /> */}
             </div>
           </div>
         </div>
